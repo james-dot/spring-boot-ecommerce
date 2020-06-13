cb991c7e9efb09c3c3de223c685fa48dee7d681a
imad <imad.live1990@gmail.com>
2020-06-13 22:15:59 +0200

    create Rest apis with spring data jpa repositories and spring data rest

---
+++ src/main/java/com/announce/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.announce.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel= "productCategory", path="product_category")
+public interface ProductCategoryRepository extends JpaRepository<ProductRepository, Long>{
+
+}
---
+++ src/main/java/com/announce/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.announce.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.announce.ecommerce.entity.Product;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
